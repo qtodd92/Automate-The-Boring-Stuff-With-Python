0efff12ee15ee3c67edc01c29bf8038bb5a9aaf2
qtodd92 <120651462+qtodd92@users.noreply.github.com>
2023-05-14 14:31:21 -0400

    Chapter 2

---
+++ Ch. 2/fiveTimes.py
@@ -0,0 +1,13 @@
+# Write your code here :-)
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' +str(i) + ')')
+
+print('')
+
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i = i + 1
+
