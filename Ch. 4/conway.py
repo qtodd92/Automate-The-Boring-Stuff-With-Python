93fedfb54ed6c8d5873a4afa43e50ebda4312e8f
qtodd92 <120651462+qtodd92@users.noreply.github.com>
2023-05-17 14:24:26 -0400

    Chapter 4

---
+++ Ch. 4/conway.py
@@ -0,0 +1,54 @@
+# Write your code here :-)
+# Conway's Game of Life
+
+import random, time, copy
+
+WIDTH = 60
+HEIGHT = 20
+
+# Create a list of list for the cells:
+nextCells = []
+for x in range(WIDTH):
+    column = [] # Create a new column.
+    for y in range(HEIGHT):
+        if random.randint(0, 1) == 0:
+            column.append('#') # Add a living cell.
+        else:
+            column.append(' ') # Add a dead cell.
+    nextCells.append(column) # nextCells is a list of column lists.
+
+while True: # Main program loop.
+    print('\n\n\n\n\n') # Separate each step with newlines.
+    currentCells = copy.deepycopy(nextCells)
+
+    # Print currentCells on the screen:
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            print(currentCells[x][y], end='') # Print the # or space.
+        print() # Print a newline at the end of the row.
+
+    #Calculate the next step's cells based on current step's cells:
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            # Get neighboring coordinates:
+            # '% WIDTH' ensures leftCoord is always between 0 and WIDTH -1
+            leftCoord  = (x - 1) % WIDTH
+            rightCoord = (x + 1) % WIDTH
+            aboveCoord = (y - 1) % HEIGHT
+
+            # Count number of living neighbors:
+            numNeighbors = 0
+            if currentCells[leftCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-left neighbor is alive.
+            if currentCells[x][aboveCoord] == '#':
+                numNeighbors += 1 # Top neighbor is alive.
+            if currentCells[rightCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-right neighbor is alive.
+            if currentCells[leftCoord][y] == '#':
+                numNeighbors += 1 # Left neighbor is alive.
+            if currentCells[rightCoord][y] == '#':
+                numNeighbors += 1 # Right neightbor is alive.
+            if currentCells[leftCoord][belowCoord] == '#':
+                numNeighbors += 1 # Bottom-left neighbor is alive.
+            if currentCells[x][belowCoord] == '#':
+
