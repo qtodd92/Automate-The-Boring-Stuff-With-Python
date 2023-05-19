2931bd563ac88903ce25c3499022b87caccdab7b
qtodd92 <120651462+qtodd92@users.noreply.github.com>
2023-05-19 16:47:18 -0400

    Update Chapter 4 and Add Chapter 5

---
+++ Ch. 4/commaCode.py
@@ -0,0 +1,10 @@
+# Write your code here :-)
+def commaCode(list1):
+    for i in range(len(list1)):
+        print(list1[i] + ',' + ' ')
+
+
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+spam2 = commaCode(spam)
+print(spam2)
